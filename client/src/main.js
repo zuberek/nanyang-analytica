2e4833d737a0806f38bb94830dd700f8a78fb2ad
Jan K Dabrowski <dabrowski.janek@gmail.com>
2019-03-25 01:24:08 +0800

    slimming

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import addIcons from './utils/icons.js'
 import 'bootstrap'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-addIcons(library)
-
-Vue.component('icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
